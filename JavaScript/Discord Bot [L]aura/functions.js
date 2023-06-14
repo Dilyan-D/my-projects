03bdbf594c643ffcf0a7def68521fbcd66759bb5
DilyanD <64197488+Dilyan-D@users.noreply.github.com>
2023-06-14 22:34:31 +0300

    Delete JavaScript/Discord Bot [L]aura directory

--- JavaScript/Discord Bot [L]aura/functions.js
+++
@@ -1,8 +0,0 @@
-// List of functions to use when needed
-
-exports.sendMessage = (msg, text, time) => {
-    msg.reply(text).then(message => {
-        setTimeout(() => message.delete(), time);
-    });
-
-};
